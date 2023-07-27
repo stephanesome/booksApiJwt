9d47814b751628f9f6d2e72fb4c6fe91ab261a9f
stephane <stephanesome@gmail.com>
2023-07-27 15:48:48 -0400

    V16

--- src/app/authentication/admin.guard.ts
+++ src/app/authentication/admin.guard.ts
@@ -2,23 +2,8 @@ import {inject} from '@angular/core';
 import {CanActivateFn} from '@angular/router';
 import {AuthenticationService} from './authentication.service';
 
-/*@Injectable({
-  providedIn: 'root'
-})
-export class AdminGuard implements CanActivate {
-  constructor(private authService: AuthenticationService) {}
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.isAdmin();
-  }
-}*/
 export const adminGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
 
   return  authService.isAdmin();
-  // if (authService.isLoggedIn()) {return true; }
-  // authService.redirectUrl = state.url;
-  // router.navigate(['./login']).then(() => {});
-  // return false;
 };
